abb3c850e0ea05230a208f719949b614bc117f07
khadimmbaye0 <khadimmbaye4002@gmail.com>
2025-03-11 16:42:09 +0000

    update

--- BACKEND/controllers/authentificationController.js
+++ BACKEND/controllers/authentificationController.js
@@ -4,31 +4,22 @@ const bcrypt = require("bcrypt");
 
 const inscription = (req, res) => {
 
-    const { email, numero, mot_de_passe } = req.body;
+    const { email, code, mot_de_passe } = req.body;
+    Utilisateur.create(email, code, mot_de_passe, (err, result) => {
 
-    Utilisateur.findByEmail(email, ( err, result ) => {
+        if (err) {
+            if(err.code === 'ER_DUP_ENTRY'){
+                return res.status(409).json({ message: "Cet email est deja associe a un compte" });
+            }
+            return res.status(500).json({ message: "Erreur survenue lors de la creation de l'utilisateur"});
+        }
+        if (result){
+            return res.status(200).json({ message: " Compte creer avec success", id: result.insertId });
+        }
+        return res.status(401).json({ message: "Vous n'etes pas autoriser a acceder a ces ressources" });
 
-       if (err) {
-           return res.status(500).json({ message: "Ereur survenue lors de la verification", error : err});
-       } if (result.length > 0) {
-           return res.status(400).json({ message: "Email deja utilisee" });
-       } else {
-            Utilisateur.create(email, numero, mot_de_passe, (err, result) => {
-
-                if (err) {
-                    if(err.code === 'ER_DUP_ENTRY'){
-                        return res.status(409).json({ message: "Cet email est deja associe a un compte" });
-                    }
-                    return res.status(500).json({ message: "Erreur survenue lors de la creation de l'utilisateur"});
-                }
-                if (result){
-                    return res.status(200).json({ message: " Compte creer avec success", id: result.insertId });
-                }
-                return res.status(401).json({ message: "Vous n'etes pas autoriser a acceder a ces ressources" });
-
-            });
-       }
     });
+
 };
 
 const connexion = async (req, res) => {
@@ -57,7 +48,6 @@ const connexion = async (req, res) => {
 
         const verifierMDP = bcrypt.compareSync(mot_de_passe, hashedMDP);
         const verifierRole = ( role.includes(roleReceived) );
-
 
         if ( !verifierMDP || !verifierRole ) {
             return res.status(401).json({ message: "Email, Mot de passe ou role incorrect "});
